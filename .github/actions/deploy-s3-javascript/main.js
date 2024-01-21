2c1e2c2b086528da2249267e014f9bb9940e39da
j-rosario-alpt <joao-o-rosario@alticelabs.com>
2024-01-21 08:02:37 +0000

    added input to custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,12 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+
     core.notice("Hello from custom JS!!!");
+    core.notice("bucket: ${bucket}, bucket-region: ${bucketRegion}, dist-folder: ${distFolder}");
 }
 
 run();
